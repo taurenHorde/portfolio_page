1b5fbdbc3bc877bf567ed2a93ff5792c91af91bc
DoUH <0008959@naver.com>
2024-08-09 15:26:43 +0900

    기본셋팅 완료

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from './Store/store.js'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter store={store}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
